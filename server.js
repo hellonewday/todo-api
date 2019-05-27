dd9b5066802a7c5f4b905bc00f58ad5f51bfb4e0
Your Name <you@example.com>
2019-05-27 21:04:15 +0700

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var http = require('http');
 var app = require('./app');
 const PORT = process.env.PORT || 1234;
-http.createServer(app).listen(PORT,()=>{
-     console.log(`Data is served in port ${PORT}`);
-});
+http.createServer(app).listen(PORT);
